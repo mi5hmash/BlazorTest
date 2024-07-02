1bdf52f9b5ccc17ab49fc2d60cedd21523516eeb
Michał Gębicki <59212335+mi5hmash@users.noreply.github.com>
2024-07-03 01:06:03 +0200

    another test

---
+++ js/localSessionStorageManager.js
@@ -0,0 +1,39 @@
+﻿// Function to set an item in localStorage
+window.setLocalStorageItem = (k, v) => {
+    localStorage.setItem(k, v);
+};
+
+// Function to get an item from localStorage
+window.getLocalStorageItem = (k) => {
+    return localStorage.getItem(k);
+};
+
+// Function to check if an item exists in localStorage
+window.localStorageItemExists = (k) => {
+    return localStorage.getItem(k) !== null;
+};
+
+// Function to remove an item from localStorage
+window.removeLocalStorageItem = (k) => {
+    localStorage.removeItem(k);
+};
+
+// Function to set an item in sessionStorage
+window.setSessionStorageItem = (k, v) => {
+    sessionStorage.setItem(k, v);
+};
+
+// Function to get an item from sessionStorage
+window.getSessionStorageItem = (k) => {
+    return sessionStorage.getItem(k);
+};
+
+// Function to check if an item exists in sessionStorage
+window.sessionStorageItemExists = (k) => {
+    return sessionStorage.getItem(k) !== null;
+};
+
+// Function to remove an item from sessionStorage
+window.removeSessionStorageItem = (k) => {
+    sessionStorage.removeItem(k);
+};
---
+++ js/themeManager.js
@@ -0,0 +1,11 @@
+﻿(() => {
+    const themeAttr = "data-bs-theme";
+
+    window.setTheme = t => {
+        document.documentElement.setAttribute(themeAttr, t);
+    };
+
+    window.getCurrentTheme = () => {
+        return document.documentElement.getAttribute(themeAttr);
+    };
+})();
